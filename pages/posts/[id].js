15526c3a63d2edcd595e70b15f56a86421f79e0a
junqu <junjv@outlook.com>
2021-08-09 14:00:15 +0800

    fix: fix typo

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Layout from "../../components/layout";
 import {getAllPostIds, getPostData} from "../../lib/posts";
-import Head from "next/Head";
+import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from '../../styles/utils.module.css'
 
