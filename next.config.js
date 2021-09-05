01559a033c780307f24b1cffc91f1f907fe6efbe
junqu <junjv@outlook.com>
2021-09-05 12:24:29 +0800

    update: upgrade next version

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     loader: "imgix",
     path: "/blog",
+  },
+  experimental: {
+    esmExternals: true
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import Layout, {siteTitle} from "../components/layout";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import {getSortedPostsData} from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home({allPostsData}) {
-
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,28 +14,28 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd}  ${utilStyles.padding1px}`}>
         <h2>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, title, date})=>(
+          {allPostsData.map(({ id, title, date }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
-              <br/>
+              <br />
               {id}
-              <br/>
+              <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
             </li>
-            ))}
+          ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
-    props: { allPostsData }
-  }
+    props: { allPostsData },
+  };
 }
